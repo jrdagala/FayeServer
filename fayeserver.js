d008bc527478d0a07c3d0a89dca4af0a455b1f0d
dnajeff <jeff@drumbi.com>
2012-05-11 07:06:21 +0800

    commit

--- fayeserver.js
+++ fayeserver.js
@@ -2,11 +2,11 @@ var faye = require('faye'),
 	bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 60});
 	
 bayeux.bind('publish',function(clientId, channel, data) {}
-	console.log("Publish = ' + channel + ' : ' + data.text);
+	console.log('Publish = ' + channel + ' : ' + data.text);
 );
 
 bayeux.bind('subscribe', function(clientId, channel){
-	console.log("Subscriber: ' + clientId + ' - ' + channel);
+	console.log('Subscriber: ' + clientId + ' - ' + channel);
 	bayeux.getClient().publish('/channel', {
 	  text:       'Sample text message',
 	});
