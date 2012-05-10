93dafaac733d75a2bc436caa7b43a409ad69519d
papzkie <papzkie@gmail.com>
2012-05-11 05:49:39 +0800

    initial commit

---
+++ fayeserver.js
@@ -0,0 +1,15 @@
+var faye = require('faye'),
+	bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 60});
+	
+bayeux.bind('publish',function(clientId, channel, data) {}
+	console.log("Publish = ' + channel + ' : ' + data.text);
+);
+
+bayeux.bind('subscribe', function(clientId, channel){
+	console.log("Subscriber: ' + clientId + ' - ' + channel);
+	bayeux.getClient().publish('/channel', {
+	  text:       'Sample text message',
+	});
+});
+bayeux.listen(8000);
+
